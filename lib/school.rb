702ba2e37897c3331d207cbd7304230a091880c2
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-30 12:05:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+  attr_accessor :roster, :grade
+  attr_reader :name, :student
+ 
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+  def add_student(name,num)
+    @roster[num]
+     
+  
+    
+  end
+
